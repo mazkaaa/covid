845ad8145aaa1fdd46046367f1d0fe5026a39e44
Muhammad Azka <azkabdg@gmail.com>
2020-04-23 00:22:37 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,236 @@
+var alreadyChoose = "choosen"
+var smoker = "smoker"
+
+let founded = false;
+let played = false;
+
+let durationExpired = 60000
+
+var sound
+
+
+//let scene = this.el.sceneEl
+/*scene.addEventListener('realityready', () => {
+	if (!isContainExpiry(alreadyChoose)){
+		//document.querySelector(".progress-bar").setAttribute('style', 'width: 100%')
+		//$(".preload").removeClass("open");
+	}
+})
+scene.addEventListener('realityerror', (event) => {
+	if (XR8.XrDevice.isDeviceBrowserCompatible()) {
+		// Browser is compatible. Print the exception for more information.
+		console.log(event.detail.error)
+		return
+	}
+
+	// Browser is not compatible. Check the reasons why it may not be.
+	for (let reason of XR8.XrDevice.incompatibleReasons()) {
+		// Handle each XR8.XrDevice.IncompatibilityReasons
+	}
+})*/
+
+function runLoadingbar(){
+	$(".preload").addClass("open")
+	document.querySelector(".progress-bar").setAttribute('style', 'width: 0%')
+	setTimeout(function() {
+		document.querySelector(".progress-bar").setAttribute('style', 'width: 10%')
+	}, 5000)
+	setTimeout(function() {
+		document.querySelector(".progress-bar").setAttribute('style', 'width: 30%')
+	}, 10000)
+	setTimeout(function() {
+		document.querySelector(".progress-bar").setAttribute('style', 'width: 50%')
+	}, 15000)
+	setTimeout(function() {
+		document.querySelector(".progress-bar").setAttribute('style', 'width: 70%')
+	}, 25000)
+}
+
+function setWithExpiry(key, value, ttl) {
+	const now = new Date()
+
+	const item = {
+		value: value,
+		expiry: now.getTime() + ttl
+	}
+	localStorage.setItem(key, JSON.stringify(item))
+}
+
+function getWithExpiry(key){
+	const itemStr = localStorage.getItem(key)
+	if (!itemStr){
+		return null
+	}
+	const item = JSON.parse(itemStr)
+	const now = new Date()
+	if (now.getTime() > item.expiry){
+		localStorage.removeItem(key)
+		return null
+	}
+	return item.value
+}
+
+function setExpire(key){
+	const itemStr = localStorage.getItem(key)
+	if (!itemStr){
+		return null
+	}
+	const item = JSON.parse(itemStr)
+	const now = new Date()
+	if (now.getTime() > item.expiry){
+		localStorage.removeItem(key)
+	}
+
+}
+
+function isContainExpiry(key){
+	let result = false
+	if (key in localStorage){
+		result = true
+	} else {
+		result = false
+	}
+	return result
+}
+
+window.onload = () => {
+	Howler.autoUnlock = false
+	setExpire(alreadyChoose)
+	if (isContainExpiry(alreadyChoose)){
+		switch (localStorage.getItem(smoker)){
+		case "true":
+			sound = new Howl({
+				src: ['audio-kotor.mp3'],
+				preload: true,
+				loop: true,
+				volume: 1,
+				onend: function () {
+					console.log('Audio finished');
+				}
+			})
+			break;
+		case "false":
+			sound = new Howl({
+				src: ['audio-sehat.mp3'],
+				preload: true,
+				loop: true,
+				volume: 1,
+				onend: function () {
+					console.log('Audio finished');
+				}
+			})
+			break;
+		}
+	} else {
+		//runLoadingbar()
+	}
+}
+
+
+function buttonSmoker(){
+	localStorage.setItem(smoker, "true")
+	//localStorage.setItem(alreadyChoose, "true")
+	setWithExpiry(alreadyChoose, "true", durationExpired)
+	window.location.replace('./index.html')
+}
+
+function buttonNonSmoker(){
+	localStorage.setItem(smoker, "false")
+	//localStorage.setItem(alreadyChoose, "true")
+	setWithExpiry(alreadyChoose, "true", durationExpired)
+	window.location.replace('./index.html')
+}
+
+AFRAME.registerComponent('paruparu', {
+	schema: {
+		name: {type: 'string'}
+	},
+	init: function () {
+
+		const {object3D, sceneEl} = this.el
+
+		// Hide the image target until it is found
+		object3D.visible = false
+
+		const paruObject = document.createElement('a-entity')
+        
+
+		if (isContainExpiry(alreadyChoose)){
+			paruObject.setAttribute('scale', '8 8 8')
+			paruObject.setAttribute('position', '0 -1 0')
+			paruObject.setAttribute('emissive', '#f5f5f5')
+			paruObject.setAttribute('emissiveIntensity', '1')
+			switch (localStorage.getItem(smoker)){
+			case "true":
+				paruObject.setAttribute('gltf-model', '#kotor-model')
+				break;
+			case "false":
+				paruObject.setAttribute('gltf-model', '#sehat-model')
+				break;
+			}
+			this.el.appendChild(paruObject)
+            
+		}
+
+
+		// showImage handles displaying and moving the virtual object to match the image
+		const showImage = ({detail}) => {
+
+			if (isContainExpiry(alreadyChoose)){
+				if (!played){
+					switch (localStorage.getItem(smoker)){
+					case "true":
+						paruObject.setAttribute('animation-mixer', 'clip: motion; loop: repeat')
+						break
+					case "false":
+						paruObject.setAttribute('animation-mixer', 'clip: motion; loop: repeat')
+						break
+					}
+					played = true
+				}
+				if (!founded){
+					sound.play()
+					founded = true
+				}
+
+				// Updating position/rotation/scale using object3D is more performant than setAttribute
+				object3D.position.copy(detail.position)
+				object3D.quaternion.copy(detail.rotation)
+				object3D.scale.set(detail.scale, detail.scale, detail.scale)
+				object3D.visible = true
+			} else {
+				window.location.replace('./option.html')
+			}
+		}
+
+		// hideImage handles hiding the virtual object when the image target is lost
+		const hideImage = () => {
+			if (played){
+				paruObject.removeAttribute('animation-mixer')
+				played = false
+			}
+			if (founded){
+				sound.stop()
+				founded = false
+			}
+
+			object3D.visible = false
+		}
+
+		// These events are routed and dispatched by xrextras-generate-image-targets
+		this.el.addEventListener('xrimagefound', showImage)
+		this.el.addEventListener('xrimageupdated', showImage)
+		this.el.addEventListener('xrimagelost', hideImage)
+	}
+})
+
+// xrextras-generate-image-targets uses this primitive to automatically populate multiple image targets
+AFRAME.registerPrimitive('paruparu-ktp', {
+	defaultComponents: {
+		paruparu: {},
+	},
+
+	mappings: {
+		name: 'paruparu.name'
+	}
+})
